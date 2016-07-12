2f703ffd19db37c7f136025896b51ca09a14433b
Bratela <den.mitrofanov@gmail.com>
2016-07-12 23:05:33 +0300

    Add Adress model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712182851) do
+ActiveRecord::Schema.define(version: 20160712195928) do
+
+  create_table "adresses", force: :cascade do |t|
+    t.string   "city"
+    t.integer  "apartment"
+    t.integer  "home"
+    t.string   "street"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "client_adresses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
